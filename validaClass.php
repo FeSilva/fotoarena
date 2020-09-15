6818f218946c49febaf31482ead941c4477fc4f3
Felipe Silva <felipe__silva@outlook.com>
2020-09-15 18:33:04 -0300

    Update validaClass.php

--- validaClass.php
+++ validaClass.php
@@ -3,10 +3,10 @@ Class ValidaUsuario
 {
 	function Validar($form){
 		$arg = [
-			empty($form['nome']) ? "Por Favor Preencha o campo de Nome corretamente." : $form['nome'],
-			filter_var($form['email'], FILTER_VALIDATE_EMAIL)	?  $form['email'] :  "Por Favor Preencha o campo de E-mail corretamente.",
-			strlen($form['senha']) <= '3' 						? "A senhas deve ser maior que 3 caracteres" :  $form['senha'],
-			$form['senha'] != $form['conf_pass']				? "As senhas não são iguais" :  $form['senha']
+			empty($form['nome']) 					? "Por Favor Preencha o campo de Nome corretamente." 	: $form['nome'],
+			filter_var($form['email'], FILTER_VALIDATE_EMAIL)	?  $form['email'] 				 	: "Por Favor Preencha o campo de E-mail corretamente.",
+			strlen($form['senha']) <= '3' 				? "A senhas deve ser maior que 3 caracteres" 		: $form['senha'],
+			$form['senha'] != $form['conf_pass']			? "As senhas não são iguais" 				: $form['senha']
 		];
 
 		for($i = 0; $i < count($arg); $i++){
