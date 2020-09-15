1c8028421918401c084ea57b4b7f6660d534ce6b
Felipe Silva <felipe__silva@outlook.com>
2020-09-15 16:58:27 -0300

    Classe de validação

---
+++ validaClass.php
@@ -0,0 +1,28 @@
+<?php
+Class ValidaUsuario
+{
+
+	function Validar($form){
+		$arg = [
+			empty($form['nome']) ? "Por Favor Preencha o campo de Nome corretamente." : $form['nome'],
+			filter_var($form['email'], FILTER_VALIDATE_EMAIL)	?  $form['email'] :  "Por Favor Preencha o campo de E-mail corretamente.",
+			$form['senha'] != $form['conf_pass'] ? "As senhas não são iguais" :  $form['senha']
+		];
+
+		if(!in_array($arg[0],$form)){
+			return $arg[0];
+		}
+		
+		if(!in_array($arg[1],$form)){
+			return $arg[1];
+		}
+		
+		if(!in_array($arg[2],$form)){
+			return "As senhas não são iguais";
+		}
+	
+		return json_encode($arg);
+	}
+}
+
+?>
