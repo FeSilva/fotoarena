acdaf266f0f612c80e6dd873667818c89575b687
Felipe Silva <felipe__silva@outlook.com>
2020-09-15 18:41:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,8 @@ $validar = new ValidaUsuario;
 
 $_POST = json_decode(file_get_contents('php://input'));
 
-  $dados = array(
-    	"nome"   			=> $_POST->nome,
-	"email"				=> $_POST->email,
-	"senha"				=> !empty($_POST->senha) 	 ? $_POST->senha :'',
-	"conf_pass"			=> !empty($_POST->confirmar) ? $_POST->confirmar : '',
-);
+$return = $validar->Validar($_POST);
 
-$return = $validar->Validar($dados);
 var_dump($return);
 
 
