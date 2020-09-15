811998d6b7f70d0f40fd60b5329756d4330f5402
Felipe Silva <felipe__silva@outlook.com>
2020-09-15 16:58:11 -0300

    Index onde recebe os dados

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+require_once "validaClass.php";
+
+$validar = new ValidaUsuario;
+
+$_POST = json_decode(file_get_contents('php://input'));
+
+  $dados = array(
+    'nome'   				=> $_POST->nome,
+	'email'				=> $_POST->email,
+	"senha"				=> !empty($_POST->senha) 		 ? md5($_POST->senha) 		 :'',
+	"conf_pass"		=> !empty($_POST->confirmar) ? md5($_POST->confirmar) : '',
+);
+
+
+
+$return = $validar->Validar($dados);
+var_dump(json_decode($return));
+
+
